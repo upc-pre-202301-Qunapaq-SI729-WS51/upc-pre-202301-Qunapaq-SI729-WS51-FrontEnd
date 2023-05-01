a0ebcb906712ac97955869efd86e1bd48c9b4cc0
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-05-01 15:27:08 -0500

    Versión incial página campaña
    -Carta lateral de donación.
    -Resumen y descripción de la campaña
    -Progressbar añadido

--- Zenquna/src/app/app.module.ts
+++ Zenquna/src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+//Para campaign
+import { MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatInputModule } from '@angular/material/input';
+
+import { MatRadioModule } from '@angular/material/radio';
+
+import { CampaignComponent } from './components/campaign/campaign.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampaignComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+    CommonModule,
+    MatRadioModule,
+    //campaign
+    MatProgressBarModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
