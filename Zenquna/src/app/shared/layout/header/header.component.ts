90c40b41edb4512c7be55dc7565c46b7b522c65c
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-05-20 17:06:42 -0500

    Breadcrumb implementado en el header

--- Zenquna/src/app/shared/layout/header/header.component.ts
+++ Zenquna/src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
+  breadcrumb: string[] = [];
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.generateBreadcrumb();
+      }
+    });
+  }
 
+  generateBreadcrumb() {
+    this.breadcrumb = [];
+
+    const urlSegments = this.router.url.split('/');
+    urlSegments.forEach((segment) => {
+      if (segment !== '') {
+        this.breadcrumb.push(segment);
+      }
+    });
+  }
+
+  getRouterLink(segment: string): string[] {
+    return [`/${segment}`];
+  }
 }
