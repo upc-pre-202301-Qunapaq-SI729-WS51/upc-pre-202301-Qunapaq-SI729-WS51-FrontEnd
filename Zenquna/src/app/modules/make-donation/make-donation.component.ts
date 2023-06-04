1f61371374dd31b2d2b9654600de0e0cc132deef
Jose Vara <josecarlosvaraquispe@gmail.com>
2023-06-04 17:09:38 -0500

    mapa integrado

--- Zenquna/src/app/modules/make-donation/make-donation.component.ts
+++ Zenquna/src/app/modules/make-donation/make-donation.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
+import { Map, NavigationControl, Marker } from 'maplibre-gl';
 
 @Component({
   selector: 'app-make-donation',
   templateUrl: './make-donation.component.html',
-  styleUrls: ['./make-donation.component.scss']
+  styleUrls: ['./make-donation.component.scss'],
 })
-export class MakeDonationComponent {
+export class MakeDonationComponent implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
 
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=dePLfbJjEjsTFDj8I5jK`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+    this.map.addControl(new NavigationControl({}), 'top-right');
+    new Marker({ color: '#FF0000' })
+      .setLngLat([139.7525, 35.6846])
+      .addTo(this.map);
+  }
+  ngOnDestroy() {
+    this.map?.remove();
+  }
 }
