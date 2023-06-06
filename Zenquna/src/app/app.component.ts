4a30606c2d5458ccef542c30051666b109bd5c1b
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-06-06 16:45:45 -0500

    arreglos generales de vista

--- Zenquna/src/app/app.component.ts
+++ Zenquna/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Zenquna';
+  isHiddenComponent: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
     this.http.get('http://localhost:3000/campaign').subscribe((data) => {
@@ -22,6 +24,17 @@ export class AppComponent {
 
     this.http.get('http://localhost:3000/comments').subscribe((data) => {
       console.log('Comments:', data);
+    });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const route = event.urlAfterRedirects;
+
+        this.isHiddenComponent =
+          route.includes('/sign-in') ||
+          route.includes('/create-account') ||
+          route.includes('/retrieve-account');
+      }
     });
   }
 }
