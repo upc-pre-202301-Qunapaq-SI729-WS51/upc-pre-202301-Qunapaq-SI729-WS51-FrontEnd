a534e86834465a6d9b5e628164dd81f3cc138660
Gianfranco4991 <u201824343@upc.edu.pe>
2023-06-22 23:45:02 -0500

    Models update

--- Zenquna/src/app/models/campaign.model.ts
+++ Zenquna/src/app/models/campaign.model.ts
@@ -1,15 +1,23 @@
 export interface Campaign {
   id: number;
+  name: string;
+  slogan: string;
+  description: string;
+  goal: number;
+  collected: number;
+  startDate: LocalDate;
+  endDate: LocalDate;
+  status: string;
+  headerImage: string;
+  bodyImage: string;
+
+
   category: Array<string>;
   department: string;
   district: string;
-  name: string;
   summary: string;
-  description: string;
   imageBanner: string;
   imageMini: string;
-  goal: number;
-  collected: number;
   donors: Array<string>;
   deadline: string;
 }
