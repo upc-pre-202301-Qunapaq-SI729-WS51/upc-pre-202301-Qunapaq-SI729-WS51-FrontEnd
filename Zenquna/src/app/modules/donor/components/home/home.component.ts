dad96081d230ed869c4020718d05d6fd6b2d0926
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-06-27 09:22:18 -0500

    Cambios por examinar

--- Zenquna/src/app/modules/donor/components/home/home.component.ts
+++ Zenquna/src/app/modules/donor/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'environments/environment.prod';
 import { Campaign } from 'src/app/core/models/campaign.model';
+import { Observable } from 'rxjs';
 
 interface Trend {
   title: string;
@@ -13,7 +14,37 @@ interface Trend {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
+@Injectable({
+  providedIn: 'root',
+})
 export class HomeComponent implements OnInit {
+  trends: Trend[] = [
+    {
+      title: 'Tendencia 1',
+      imageUrl: 'assets/trend1.jpg',
+    },
+    {
+      title: 'Tendencia 2',
+      imageUrl: 'assets/trend2.jpg',
+    },
+    {
+      title: 'Tendencia 3',
+      imageUrl: 'assets/trend3.jpg',
+    },
+    {
+      title: 'Tendencia 4',
+      imageUrl: 'assets/trend4.jpg',
+    },
+    {
+      title: 'Tendencia 5',
+      imageUrl: 'assets/trend5.jpg',
+    },
+    {
+      title: 'Tendencia 6',
+      imageUrl: 'assets/trend6.jpg',
+    },
+  ];
+  campaign1: Campaign | undefined;
   campaigns!: any[];
 
   constructor(private http: HttpClient) {}
@@ -31,12 +62,13 @@ export class HomeComponent implements OnInit {
   }
 
   fetchCampaigns() {
-    const apiUrl = 'https:localhost:8080/api/zq/v1/campaigns';
+    const apiUrl = 'https://zenquna.uc.r.appspot.com/api/zq/v1';
     const headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
 
     this.http.get<any[]>(apiUrl, { headers }).subscribe(
       (campaigns) => {
         this.campaigns = campaigns;
+        this.campaign1 = campaigns[0];
       },
       (error) => {
         console.error('Error fetching campaigns:', error);
